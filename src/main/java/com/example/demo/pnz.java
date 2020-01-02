7377593f2ef28971fd2ea05c28813cc59baad555
yourUsername <yourEmail@domain.com>
2020-01-02 15:08:22 -0500

    bg

---
+++ src/main/java/com/example/demo/pnz.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+public class pnz {
+    public static void main(String[] args) {
+
+        int h = 1;
+
+     while(h<=10) {
+     if(h==5) {
+         h++;
+         break;
+     }
+System.out.println( "k"+h );
+  h++;
+
+     }
+     }
+
+    }
